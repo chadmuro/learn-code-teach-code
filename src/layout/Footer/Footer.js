a0b606953652e353c05064ddec8aeae013f18c76
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-12-13 22:54:56 +0900

    create footer

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer">
+            <p>Copyright &copy; 2020 Chad Murobayashi</p>
         </div>
     )
 }
 
-export default Footer
+export default Footer;
